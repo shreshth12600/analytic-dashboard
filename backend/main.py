52b239d04d5621f7cd502626024826df11fb37e4
Shreshth Jain <shreshth12jn@gmail.com>
2026-06-14 22:25:01 +0530

    Added Devextreme

--- backend/main.py
+++ backend/main.py
@@ -1,32 +1,21 @@
 from fastapi import FastAPI
-from backend.queries import kpi_overview, trend_overview, efficiency_overview,log_overview
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/")
-def start():
-    return{"message": "Backend Started"}
+# your routes here
 
-@app.get("/api/overview/kpis")
-def get_kpi_overview():
-    return kpi_overview()
-
-@app.get("/api/overview/trends")
-def get_trend_overview():
-    return trend_overview()
-
-@app.get("/api/overview/efficiency")
-def get_efficiency_trend():
-    return efficiency_overview()
-
-@app.get("/api/overview/logs")
-def get_log_overview():
-    return log_overview()
+app.mount(
+    "/",
+    StaticFiles(directory="frontend", html=True),
+    name="frontend"
+)
